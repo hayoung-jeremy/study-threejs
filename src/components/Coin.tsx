59b2f8d5ee3efbe8ef6cf16adbbe6a3e8bc87b76
Ha young, Kim <jeremiah91@naver.com>
2022-04-26 10:58:51 +0900

    feat : light

--- src/components/Coin.tsx
+++ src/components/Coin.tsx
@@ -8,7 +8,7 @@ const Coin = () => {
   const mesh = useRef<THREE.Mesh>(null);
   useFrame(() => (mesh.current!.rotation.y = mesh.current!.rotation.z += 0.01));
   return (
-    <mesh ref={mesh} scale={0.7}>
+    <mesh ref={mesh} scale={1}>
       <cylinderBufferGeometry args={[1, 1, 0.3, 50]} />
       <meshLambertMaterial attach='material' color='#ff9800' />
     </mesh>
--- src/components/TestCanvas.tsx
+++ src/components/TestCanvas.tsx
@@ -1,12 +1,19 @@
 // three
 import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Stars } from '@react-three/drei';
+
+// custom
 import Coin from './Coin';
 
 const TestCanvas = () => {
   return (
     <>
       <Canvas>
-        <Coin></Coin>
+        <OrbitControls />
+        <Stars />
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 10, 10]} angle={0.5} />
+        <Coin />
       </Canvas>
     </>
   );
